361b44c0e1fc9439c89f1b01122f5fbe5e1a3c87
mohamedbenarfa1234 <medbenarfamed@gmail.com>
2023-06-07 10:43:40 +0100

    step one

---
+++ assets/js/main.js
@@ -0,0 +1,151 @@
+
+
+var easy = [
+    {
+      question: "Which planet is known as the 'Red Planet'?",
+      answers: [
+        {text: "Jupiter", answerValid: false},
+        {text: "Mars", answerValid: true},
+        {text: "Venus", answerValid: false}
+      ]
+    },
+
+    {
+        question: "What is the capital city of France?",
+        answers: [
+          {text: "Rome", answerValid: false},
+          {text: "Paris", answerValid: true},
+          {text: "Madrid", answerValid: false}
+        ]
+      },
+
+      {
+        question: "Who painted the Mona Lisa?",
+        answers: [
+          {text: "vincent van Gogh", answerValid: false},
+          {text: "leonardo de Vinci", answerValid: true},
+          {text: "Pablo Picasso", answerValid: false}
+        ]
+      },
+    
+      {
+        question: "What is the largest ocean in the world?",
+        answers: [
+          {text: "Indian Ocean", answerValid: false},
+          {text: "Atlantic Ocean", answerValid: false},
+          {text: "Pacific Ocean", answerValid: true}
+        ]
+      },
+    
+      {
+        question: "Who Wrote the play Romeo and Juliet?",
+        answers: [
+          {text: "William Shakespeare", answerValid: true},
+          {text: "Jane Austen", answerValid: false},
+          {text: "Mark Twain", answerValid: false}
+        ]
+      } 
+    
+    
+]
+
+var medium = [
+      {
+        question: "In wich country was the composer Ludwing van Beethoven born?",
+        answers: [
+          {text: "Germany", answerValid: true},
+          {text: "Austria", answerValid: false},
+          {text: "Italy", answerValid: false}
+        ]
+      },
+
+ {
+    question: "Which famous scientist developed the theory of general relativity?",
+    answers: [
+      {text: "Isaac Newton", answerValid: false},
+      {text: "Albert Einshtein", answerValid: true},
+      {text: "Galileo Galilei", answerValid: false}
+    ]
+  } ,
+
+  {
+    question: "What is the world's longest river?",
+    answers: [
+      {text: "Nile River", answerValid: true},
+      {text: "Amazon River", answerValid: false},
+      {text: "Yangtze River", answerValid: false}
+    ]
+  } ,
+
+
+  {
+    question: "Which famous artist is known for painting The Starry Night?",
+    answers: [
+      {text: "Salvador Dali", answerValid: false},
+      {text: "Vincent van Gogh", answerValid: true},
+      {text: "Pablo Picasso", answerValid: false}
+    ]
+  } ,
+
+  {
+    question: "Who wrote the novel Pride and Prejudice?",
+    answers: [
+      {text: "Jane Austen", answerValid: true},
+      {text: "Emily Brontë", answerValid: false},
+      {text: "Charles Dickens", answerValid: false}
+    ]
+  } 
+]
+
+
+  var hard=[
+
+  {
+    question: "who discovered penicillin , a life-saving antibiotic?",
+    answers: [
+      {text: "Alexender Fleming", answerValid: true},
+      {text: "Marie Curie", answerValid: false},
+      {text: "Louis Pasteur", answerValid: false}
+    ]
+  } ,
+
+
+  {
+    question: "In which year did the world war II end?",
+    answers: [
+      {text: "1945", answerValid: true},
+      {text: "1939", answerValid: false},
+      {text: "1941", answerValid: false}
+    ]
+  } ,
+
+
+  {
+    question: "who is considered the 'Father of Modern physics?",
+    answers: [
+      {text: "Max Planck", answerValid: false},
+      {text: "Isaac Newton", answerValid: false},
+      {text: "Albert Einstein", answerValid: true}
+    ]
+  } ,
+
+
+  {
+    question: "what is the chemical symbol for the element gold?",
+    answers: [
+      {text: "Au", answerValid: true},
+      {text: "Ag", answerValid: false},
+      {text: "Fe", answerValid: false}
+    ]
+  } ,
+
+
+  {
+    question: "which famous scientist formulated the laws of motion ?",
+    answers: [
+      {text: "Isaac Newton", answerValid: true},
+      {text: "Galileo Galilei", answerValid: false},
+      {text: "Nikola Tesla", answerValid: false}
+    ]
+  } 
+]
